82e8c050337ec8210a57463202090188708584d0
Dmitry <elxf123@gmail.com>
2023-04-05 21:13:31 +0300

    add dynamic loading for page content

--- src/components/Home/HomeComponent.tsx
+++ src/components/Home/HomeComponent.tsx
@@ -1,38 +1,55 @@
-import { Box, Grid, Paper, styled, useTheme} from "@mui/material";
+import { Grid, Paper, Slide,} from "@mui/material";
+import { useInView } from 'react-intersection-observer';
+import styles from "./styles";
 import CustomFab from "../CustomFab/CustomFab";
-import CustomNavbar from "../CustomNavbar/CustomNavbar";
-import CustomTimeline from "../CustomTimeline/CustomTimeline";
-import FlexDrawer from "../FlexDrawer/FlexDrawer";
-import ListBlock from "../ListBlock/ListBlock";
-import MapComponent from "../MapComponent/MapComponent";
-import MapControlComponent from "../MapControlComponent/MapControlComponent";
 import NavbarMargin from "../NavbarMargin/NavbarMargin";
 import NewsBlock from "../NewsBlock/NewsBlock";
-import styles from "./styles";
+import ListBlock from "../ListBlock/ListBlock";
+import CustomTimeline from "../CustomTimeline/CustomTimeline";
+import MapComponent from "../MapComponent/MapComponent";
+import MapControlComponent from "../MapControlComponent/MapControlComponent";
+
 
 
 const HomeComponent = () => {
 
+    const intersectionOptions = {
+        delay: 500,
+        //triggerOnce: true,
+        trackVisibility: true,
+    };
+
+    const { ref: listBlockRef, inView: listBlockInView } = useInView(intersectionOptions);
+    const { ref: mapBlockRef, inView: mapblockInView } = useInView(intersectionOptions);
     
     return (
         <Paper sx={{ml: "3%"}}>
             <NavbarMargin />
             <Grid container spacing={2}>
                 <Grid item xs={12}>
-                    <NewsBlock />
+                     <NewsBlock />
                 </Grid>
-                <Grid item xs={9}>
+                <Slide direction="right" in={listBlockInView}>
+                <Grid item xs={9} ref={listBlockRef}>
                     <ListBlock />
                 </Grid>
-                <Grid item xs={3}>
+                </Slide>
+                <Slide direction="right" in={listBlockInView}>
+                <Grid item xs={3} ref={listBlockRef}>
                     <CustomTimeline />
+                </Grid>    
+                </Slide>
+                <Slide direction="right" in={mapblockInView}>
+                <Grid item xs={9} ref={mapBlockRef}>
+                    {mapblockInView && <MapComponent />}
                 </Grid>
-                <Grid item xs={9}>
-                    <MapComponent />
+                </Slide>
+                <Slide direction="right" in={mapblockInView}>
+                <Grid item xs={3} ref={mapBlockRef} sx={styles.mapControlBox}>
+                    {mapblockInView && <MapControlComponent />}
                 </Grid>
-                <Grid item xs={3} sx={styles.mapControlBox}>
-                    <MapControlComponent />
-                </Grid>
+                    
+                </Slide>
             </Grid>
             <CustomFab />
         </Paper>
